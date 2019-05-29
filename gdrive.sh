72854943b1429898825b2e3eccdbb54fc1386542
DNI9 <26569495+DNI9@users.noreply.github.com>
2019-05-30 05:16:13 +0530

    Delete gdrive.sh

--- gdrive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd ~
-wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
-mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
-chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive
-gdrive list
