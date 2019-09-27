7f3f6cc7055d7bf51fd154388c9dca3f182e5ef6
DNI9 <itsr9boi@gmail.com>
2019-09-27 20:59:28 +0530

    no need , go away

--- gdrive_cli.sh
+++ gdrive_cli.sh
@@ -5,6 +5,6 @@ sudo cp gdrive-linux-x64 /usr/local/bin/gdrive;
 sudo chmod a+x /usr/local/bin/gdrive;
 echo -n " Obtain the string behind the ?id= , and paste here it will ask for permission, do that !"
 echo -n "Enter the file id: (Timeout 30s)"
-read -t 30 fileID
+read fileID
 gdrive download $fileID
 
