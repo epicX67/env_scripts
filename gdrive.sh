42360d3949591693e04eb2664c361eaa0e66de6f
epicX67 <46811948+epicX67@users.noreply.github.com>
2019-10-05 22:13:59 +0530

    Update gdrive.sh

--- gdrive.sh
+++ gdrive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~
-wget https://github.com/DNI9/scripts/raw/master/gdrive
+wget https://github.com/epicX67/env_scripts/raw/master/gdrive
 chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
 gdrive list
